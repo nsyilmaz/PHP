11e61689e55f68d07469101b9134357833affd89
Nebi Senol YILMAZ <nsyilmaz@gmail.com>
2021-11-16 22:14:50 +0000

    fix

--- JWT/flag.php
+++
@@ -1,9 +0,0 @@
-<?php
-require "auth.php";
-$is_auth = authenticate();
-
-if ($is_auth == True) {
-	echo "HTB{" . str_replace(array("\r", "\n"), '', file_get_contents('/home/htb/user.txt')) . "}";
-} else {
-	echo("You are not admin");
-}
