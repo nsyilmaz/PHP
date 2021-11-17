f4136de427afb0e490944cd95bdab001e34d424b
Nebi Senol YILMAZ <nsyilmaz@gmail.com>
2021-11-17 00:50:24 +0000

    new

--- slscan.sh
+++ slscan.sh
@@ -3,9 +3,14 @@
 #
 # Shiftleft scan
 #
-# docker run --name shiftleftscanlatest_650bc3 --label e28490 --workdir /github/workspace --rm  -v "/Users/nsyilmaz/SourceTree/PHP":"/github/workspace" shiftleft/scan:latest  "scan" "--src" "/github/workspace" "--out_dir" "reports" "--type" "" "--no-error"
 
 
+# docker run --name shiftleftscanlatest_650bc3 --label e28490 --workdir /github/workspace --rm  -v "/Users/nsyilmaz/SourceTree/tmp":"/github/workspace" shiftleft/scan:latest  "scan" "--src" "/github/workspace" "--out_dir" "reports" "--type" "php"
 
 
+# docker run --name shiftleftscanlatest_650bc3 --label e28490 --workdir /github/workspace --rm  -v "/Users/nsyilmaz/SourceTree/tmp":"/github/workspace" shiftleft/sast-scan:latest  "scan" "--src" "/github/workspace" "--out_dir" "reports" "--type" "php"
 
+
+# docker run --name shiftleftscanlatest_650bc3 --label e28490 --workdir /github/workspace --rm  -v "/Users/nsyilmaz/SourceTree/tmp":"/github/workspace" shiftleft/scan-slim:latest  "scan" "--src" "/github/workspace" "--out_dir" "reports" "--type" "php"
+
+# docker run --name shiftleftscanlatest_650bc3 --label e28490 --workdir /github/workspace --rm  -v "/Users/nsyilmaz/SourceTree/tmp":"/github/workspace" shiftleft/scan-base:latest  "scan" "--src" "/github/workspace" "--out_dir" "reports" "--type" "php"
